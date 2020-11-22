19790e209f794461b5960602f48e7daa083e897e
AstraBR <astra@gmail.com>
2020-11-22 11:07:10 -0300

    nada

---
+++ inicial.py
@@ -0,0 +1,21 @@
+###Comentários no Python
+
+print("Hello World!\n")  ###escreva na tela com pula linha
+print("Pulei uma linha") ###escreva na tela simples
+
+###Variaveis em Python
+
+nome = 'Alan'     ###String
+idade = 19        ###inteiro
+altura = 1.80     ###Float
+namorando = True  ###Bool
+
+###Frase inicial
+print(" Seu nome é ", nome, "nasceu em 06 de setembro de 2001 com idade atual de ", idade, "anos.")
+
+nome = input("Escreva seu nome: ")
+idade = input("Digite sua idade: ")
+
+###Frase usando Input
+
+print(" Seu nome é ", nome, "com ", idade, "anos.")
